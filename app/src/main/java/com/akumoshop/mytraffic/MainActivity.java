25ff3578f020194617f537c49f09f491dc5c974a
akumo <akumo@yahoo.com>
2015-11-28 14:40:20 +0700

    Start

---
+++ app/src/main/java/com/akumoshop/mytraffic/MainActivity.java
@@ -0,0 +1,22 @@
+package com.akumoshop.mytraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit  (access type name)
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }   // Main Method
+}   // Main Class นี่คือคลาสหลัก
