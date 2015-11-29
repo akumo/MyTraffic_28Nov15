a871434bf1421d0f4f5746d72bd6bcae6df1e9e6
akumo <akumo@yahoo.com>
2015-11-29 10:03:20 +0700

    สร้าง onClickListener เสร็จแล้ว

--- app/src/main/java/com/akumoshop/mytraffic/MainActivity.java
+++ app/src/main/java/com/akumoshop/mytraffic/MainActivity.java
@@ -2,6 +2,7 @@ package com.akumoshop.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,9 +21,29 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button controller
+        buttonController();
+
 
     }   // Main Method
 
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }//event
+        });
+
+    }//buttonController
+
     private void bindWidget() {
-    }
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    } //bindWidget
 }   // Main Class นี่คือคลาสหลัก
